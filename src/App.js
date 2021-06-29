cc363b2fda745a8cd26c17c7af8f9895f8b7ee3b
Kabil-N <78339317+Kabil-N@users.noreply.github.com>
2021-06-29 18:07:28 +0530

    react-redux-persists

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import CakeContainer from './components/cakeContainer';
 import {Provider} from 'react-redux';
 import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistor } from './redux/store'
 function App() {
   return (
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
       <div className="App">
         <CakeContainer />
       </div>
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,18 @@
 import {createStore } from 'redux';
 import cakeReducer from './cake/cakeReducer';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-const store=createStore(cakeReducer)
+
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+   
+const persistedReducer = persistReducer(persistConfig, cakeReducer)
+
+const store=createStore(persistedReducer)
+
+export const persistor=persistStore(store);
 
 export default store;
